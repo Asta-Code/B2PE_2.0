fb2440b42a3139b80e1d2747f4adcd58a6ed83a1
Cristofer Azaña <cristoferprograma@gmail.com>
2024-09-07 19:48:24 -0500

    createrut

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,4 @@
-import { userRegisterExpect } from "../controllers/user_controller";
+import { userRegisterExpect, usersGet } from "../controllers/user_controller";
 
 const { Router } = require('express');
 
@@ -7,8 +7,8 @@ const { Router } = require('express');
 const router = Router();
 
 router.post('/register',userRegisterExpect );
-
-
+// Crear una ruta para traer todos los users con store procedure 
+router.get('/users', usersGet )
 
 
 module.exports = router;
